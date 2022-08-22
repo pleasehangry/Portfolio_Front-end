af8bba40ad14dbd0d72b9f8e42e420d422d81473
= <=>
2022-08-22 22:06:04 +0700

    Sanity & React

---
+++ src/client.js
@@ -0,0 +1,14 @@
+import sanityClient from '@sanity/client';
+import imageUrlBuider from '@sanity/image-url';
+
+export const client = sanityClient({
+    projectId: process.env.REACT_APP__SANITY_PROJECT_ID,
+    dataset: 'production',
+    apiVersion: '2022-02-01',
+    useCdn: true,
+    token: process.env.REACT_APP__SANITY_PROJECT_TOKEN,
+});
+
+const builder = imageUrlBuider(client);
+
+export const urlFor = (source) => builder.image(source);
--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -2,31 +2,42 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import './About.scss';
 import { images } from '../../constants';
+import { urlFor, client } from '../../client';
 
-const abouts = [
-    {
-        title: 'Web Development',
-        description: 'I am a good web developer.',
-        imgUrl: images.about01,
-    },
-    {
-        title: 'Web Design',
-        description: 'I am a good web developer.',
-        imgUrl: images.about02,
-    },
-    {
-        title: 'UI/UX',
-        description: 'I am a good web developer.',
-        imgUrl: images.about03,
-    },
-    {
-        title: 'Web animation',
-        description: 'I am a good web developer.',
-        imgUrl: images.about04,
-    },
-];
+// const abouts = [
+//     {
+//         title: 'Web Development',
+//         description: 'I am a good web developer.',
+//         imgUrl: images.about01,
+//     },
+//     {
+//         title: 'Web Design',
+//         description: 'I am a good web developer.',
+//         imgUrl: images.about02,
+//     },
+//     {
+//         title: 'UI/UX',
+//         description: 'I am a good web developer.',
+//         imgUrl: images.about03,
+//     },
+//     {
+//         title: 'Web animation',
+//         description: 'I am a good web developer.',
+//         imgUrl: images.about04,
+//     },
+// ];
 
 const About = () => {
+    const [abouts, setAbouts] = useState([]);
+
+    useEffect(() => {
+        const query = '*[_type == "abouts"]';
+
+        client.fetch(query).then((data) => {
+            setAbouts(data);
+        });
+    }, []);
+
     return (
         <>
             <h2 className="head-text">
@@ -45,7 +56,7 @@ const About = () => {
                         className="app__profile-item"
                         key={about.title + index}
                     >
-                        <img src={about.imgUrl} alt={about.imgUrl} />
+                        <img src={urlFor(about.imgUrl)} alt={about.imgUrl} />
                         <h2 className="bold-text" style={{ marginTop: 20 }}>
                             {about.title}
                         </h2>
